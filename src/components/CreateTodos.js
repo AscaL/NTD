097303c089cd4469fae9f2e45215da5d29ea7ce6
AscaL <emanueleorfanelli@gmail.com>
2017-01-07 03:36:25 +0100

    kind of working todo

--- src/components/CreateTodos.js
+++ src/components/CreateTodos.js
@@ -1,12 +1,58 @@
 import React from 'react';
+import _ from 'lodash';
 
 export default class CreateTodos extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      error: null,
+    };
+  }
+
+  handleCreate(e) {
+    e.preventDefault();
+
+    // we need to get values from input box and add them to the todo array
+    const createInput = this.refs.createInput;
+    const task = createInput.value;
+    const validateInput = this.validateInput(task);
+
+    if (validateInput) {
+      this.setState({ error: validateInput });
+      return;
+    }
+
+    this.setState({ error: null });
+    this.props.createTask(task);
+    this.refs.createInput.value = '';
+  }
+
+  renderError() {
+    if (!this.state.error) {
+      return null;
+    }
+    return <div style={{color: 'red'}}>{this.state.error}</div>
+  }
+
   render() {
     return (
-      <form>
-        <input type="text" />
+      <form onSubmit={this.handleCreate.bind(this)} >
+        <input type="text" placeholder="What to do?" ref="createInput" />
         <button>Create</button>
+        {this.renderError()}
       </form>
     );
   }
+
+  validateInput(task) {
+    if (!task) {
+      return 'please enter a task!';
+    } else if (_.find(this.props.todos, todo => todo.task === task)) {
+      return 'task already exists';
+    } else {
+      return null;
+    }
+  }
+
 }
--- src/components/app.js
+++ src/components/app.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import _ from 'lodash';
 
 import TodosList from './TodosList';
 import CreateTodos from './CreateTodos';
@@ -23,11 +24,45 @@ export default class App extends React.Component {
     };
   }
 
+  toggleTask(task) {
+    const foundTodo = _.find(this.state.todos, todo => todo.task === task);
+    foundTodo.isCompleted = !foundTodo.isCompleted;
+    this.setState({ todos: this.state.todos });
+  }
+
+  createTask(task) {
+    this.state.todos.push({
+      task,
+      isCompleted: false,
+    });
+    this.setState({ todos: this.state.todos });
+  }
+
+  saveTask(oldTask, newTask) {
+    const foundTodo = _.find(this.state.todos, todo => todo.task === oldTask);
+
+    foundTodo.task = newTask;
+    this.setState({ todos: this.state.todos });
+  }
+
+  deleteTask(task) {
+    const foundTodo = _.remove(this.state.todos, todo => todo.task === task);
+    this.setState({todos: this.state.todos });
+
+
+  }
+
   render() {
     return (
       <div>
         <h1>Nested ToDo and Daily</h1>
-        <TodosList todos={this.state.todos} />
+        <CreateTodos todos={this.state.todos} createTask={this.createTask.bind(this)} />
+        <TodosList 
+          todos={this.state.todos} 
+          toggleTask={this.toggleTask.bind(this)}
+          saveTask={this.saveTask.bind(this)}
+          deleteTask={this.deleteTask.bind(this)}
+        />
       </div>
     );
   }
